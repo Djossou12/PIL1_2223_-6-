469a92baac7355eded50e108878722f18b3a37d5
odilonHNV <136507816+odilonHNV@users.noreply.github.com>
2023-06-23 01:01:09 +0200

    ajout des fichers de connection
    ajout de la page de connexion et de la page d'inscription au fichier views.py de django avec leurs mises en relation.

---
+++ views.py
@@ -0,0 +1,30 @@
+from ssl import HAS_TLSv1_1
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import User
+from admins.models import Band
+
+def hello(request):
+    bands = Band.objects.all()
+    return render(request,'bands/hello.html',{'bands':bands})
+
+def about(request):
+    return HttpResponse('<h1>A propos</h1> <p> nous adorons</p> <header class="de"> <div class="titl"><h1>ODISHOP</h1>pour vos acessoires</div><nav class="nav"><ul><li><a href="Accueil.html" class="acc">Accueil</a></li><li><a href="article.html" class="art">Catalogue</a></li><li><a href="Connexion.html" class="con">Se connecter</a></li></ul></nav></header>')
+def text (request):
+    return HttpResponse('<div class="aire1"><section id="con">  <h1>ODISHOP</h1><form action="contact">    <div class="oup">      <label for="name"></label>  <input type="text" placeholder="Entrer votre nom ou votre email" required/></div><div class="oup"> <label for="pasword"></label><input type="pasword" name="mot de passe" placeholder="Entrer le mot de passe" required/></div><div style="padding-left: 275px;"> <a href="">Mot de passe oublier?</a> </div><div classe="oup"> <button type="submit">Se connecter</button></div></form><div id="fot"> Vous navaez pas de compte? <a href="">Inscrivez-vous</a> </div></section> </div>')
+
+def inscription(request):
+    return render(request,'listings/inscription.html')
+def login(request):
+    return render(request,'listings/login.html')
+
+def user(request):
+    return render(request,'listings/user.html',{})
+
+def insertuser(request):
+    vuid = request.POST['tuid'];
+    vuname = request.POST['tuname'];
+    vuemail = request.POST['tuemail'];
+    us=User(uid=vuid, uname=vuname, uemail=vuemail);
+    us.save();
+    return render(request,'listings/hello.html',{});
