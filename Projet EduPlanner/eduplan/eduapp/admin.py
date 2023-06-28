75b8cd5fc5cc101ba6715a81e0948f1ad5f1fab3
Tobi3h <136495514+Tobi3h@users.noreply.github.com>
2023-06-28 13:08:05 +0200

    Delete Projet EduPlanner/eduplan directory

--- Projet EduPlanner/eduplan/eduapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Licence1IA
-
-admin.site.register(Licence1IA)
--- Projet EduPlanner/eduplan/eduapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Licence1IA(models.Model): 
-    id = models.IntegerField(unique=True,primary_key=True)
-    Jours = models.CharField(max_length=64, unique=True)
-    # Matières = models.CharField(max_length=64, null=True)
-    # Heures = models.CharField(max_length=64, null=True)
-
